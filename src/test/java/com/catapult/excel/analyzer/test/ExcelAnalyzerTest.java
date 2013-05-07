b12034bfa60b6e0665dccf0ef6758ebe45aac1c3
Jayrome Vergara <jaycverg@gmail.com>
2013-05-07 10:51:03 +0800

    added analyzer test ui

--- src/test/java/com/catapult/excel/analyzer/test/ExcelAnalyzerTest.java
+++ src/test/java/com/catapult/excel/analyzer/test/ExcelAnalyzerTest.java
@@ -3,6 +3,8 @@ package com.catapult.excel.analyzer.test;
 import com.catapult.excel.analyzer.ExcelDataHeader;
 import com.catapult.excel.analyzer.ExcelSheetAnalyzer;
 import java.io.InputStream;
+import javax.swing.JDialog;
+import javax.swing.UIManager;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
@@ -16,15 +18,28 @@ import org.junit.Test;
 public class ExcelAnalyzerTest
 {
     @Test
+    public void testWithUI()
+    {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch(Exception e){}
+        
+        JDialog d = new JDialog();
+        d.setTitle("Test Excel Sheet Analyzer");
+        d.setContentPane(new TestUI());
+        d.pack();
+        d.setModal(true);
+        d.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        d.setVisible(true);
+    }
+
+    //@Test
     public void test()
     {
         InputStream is = null;
         try
         {
-//            is = getClass().getResourceAsStream("format4.xlsx");
-//            Workbook wb = WorkbookFactory.create(is);
-//            Sheet sheet = wb.getSheetAt(wb.getSheetIndex("Rates"));
-
             is = getClass().getResourceAsStream("format2.xlsx");
             Workbook wb = WorkbookFactory.create(is);
             Sheet sheet = wb.getSheetAt(0);
